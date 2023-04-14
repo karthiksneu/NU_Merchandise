9be6012d6b1faf052b8ea783c283aac8e2f7f009
Freya Dsouza <113114228+Freya2597@users.noreply.github.com>
2023-04-14 09:52:01 -0400

    create customer stored procedure

--- Create_Table_Scripts.sql
+++ Create_Table_Scripts.sql
@@ -593,21 +593,42 @@ ORDER BY designation, employee_name;
 COMMIT;
 
 --Stored Procedures
-
-create or replace PROCEDURE AddUsers(
-Customer_Id in Number,
-first_Name in VARCHAR2,
-last_name in VARCHAR2,
-Email_Id in VARCHAR2,
-Phone_Number in VARCHAR2,
-Customer_Type in VARCHAR2
-)
-IS
+-- Create new customer
+create or replace PROCEDURE create_newcustomer(
+  p_customer_id IN customer.customer_id%TYPE,
+  p_first_name IN customer.first_name%TYPE,
+  p_last_name IN customer.last_name%TYPE,
+  p_email_id IN customer.email_id%TYPE,
+  p_phone_number IN customer.phone_number%TYPE,
+  p_customer_type IN customer.customer_type%TYPE
+) AS
+  l_count NUMBER;
 BEGIN
-INSERT INTO Customer	
-VALUES (Customer_Id,first_Name,last_name,Email_Id,Phone_Number,Customer_Type);
-commit;
+  -- Check if customer already exists
+  SELECT COUNT(*) INTO l_count FROM customer WHERE customer_id = p_customer_id;
+  IF l_count > 0 THEN
+    RAISE_APPLICATION_ERROR(-20001, 'Customer already exists.');
+  ELSE
+    -- Perform validation on customer data
+    IF p_first_name IS NULL OR p_last_name IS NULL OR p_email_id IS NULL OR p_phone_number IS NULL OR p_customer_type IS NULL THEN
+      RAISE_APPLICATION_ERROR(-20002, 'Invalid customer data.');
+      dbms_output.put_line('Invalid customer data.');
+    ELSE
+      -- Insert new customer into the table
+      INSERT INTO customer (customer_id, first_name, last_name, email_id, phone_number, customer_type)
+      VALUES (p_customer_id, p_first_name, p_last_name, p_email_id, p_phone_number, p_customer_type);
+      dbms_output.put_line('New customer created successfully.');
+      COMMIT;
+
+    END IF;
+  END IF;
+EXCEPTION
+  -- Handle exceptions
+  WHEN OTHERS THEN
+    ROLLBACK;
+    dbms_output.put_line('Errors: ' || SQLCODE || ' - ' || SQLERRM);
 END;
+
 
 -- Update procedure to update availability of quantity
 
